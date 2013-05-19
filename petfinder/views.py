7181d111eb922cc8e2f9582a3206da6b6f571d1b
Sergey Leschina <mail@putnik.ws>
2013-05-19 14:37:05 +0400

    Add search

--- petfinder/views.py
+++ petfinder/views.py
@@ -7,7 +7,29 @@ from petfinder.models import *
 def home(request):
     petphotos = PetPhoto.objects.select_related()
 
+    city = request.GET['city']
+    if (city):
+        petphotos = petphotos.filter(pet__city=city)
+
+    kind = request.GET['kind']
+    if (kind):
+        petphotos = petphotos.filter(pet__kind=kind)
+
+    age = request.GET['age']
+    if (age):
+        if age == '0-1':
+            petphotos = petphotos.filter(pet__age__lte=1)
+        elif age == '0-1':
+            petphotos = petphotos.filter(pet__age__gte=1, pet__age__lte=3)
+        elif age == '3+':
+            petphotos = petphotos.filter(pet__age__gte=3)
+
+    sex = request.GET['sex']
+    if (sex):
+        petphotos = petphotos.filter(pet__sex=sex)
+
     return render_to_response('home.html', {
+        'request':  request,
         'petphotos': petphotos,
         }, context_instance=RequestContext(request))
 
