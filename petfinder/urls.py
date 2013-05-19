7abdd62e4d298c4d9b0ed0299639c82416628952
Sergey Leschina <mail@putnik.ws>
2013-05-19 13:26:47 +0400

    Add photos gallery

--- petfinder/urls.py
+++ petfinder/urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'petfinder.views.home', name='home'),
+    url(r'^pet/(?P<id>\d+)/?$', 'petfinder.views.pet', name='pet'),
     # url(r'^petfinder/', include('petfinder.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- petfinder/views.py
+++ petfinder/views.py
@@ -5,8 +5,17 @@ from petfinder.models import *
 
 
 def home(request):
+    petphotos = PetPhoto.objects.select_related()
+
     return render_to_response('home.html', {
+        'petphotos': petphotos,
         }, context_instance=RequestContext(request))
 
+def pet(request, id):
+    pet = PetPhoto.objects.get(id=id)
+    photos = PetPhoto.objects.filter(pet=pet)
 
+    return render_to_response('pet.html', {
+        'photos': photos,
+        }, context_instance=RequestContext(request))
 
